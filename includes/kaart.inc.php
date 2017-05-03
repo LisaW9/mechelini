8f34bf0feb35af650417583ba92c6eb3b3affe09
Lisa9W <lisa@m-release.com>
2017-05-03 11:57:19 +0200

    Started ruilplaza screens

---
+++ includes/kaart.inc.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" type="text/css" href="../css/reset.css">
+<link rel="stylesheet" type="text/css" href="../css/kaart.css">
+
+<div class="kaart">
+    <div class="kaartAfbeelding"></div>
+    <h1 class="kaartNaam">Elias Cobbaut</h1>
+    <h2 class="kaartThema">Spelers</h2>
+</div>
--- includes/ruilkaart.inc.php
+++ includes/ruilkaart.inc.php
@@ -1,8 +1,9 @@
+<link rel="stylesheet" type="text/css" href="../css/ruilkaart.css">
 <div class="ruilkaart">
     <?php include_once('kaart.inc.php'); ?>
     <div class="info">
         <div class="user">
-            <div class="profilePicture" style="background-image"></div>
+            <div class="profilePicture" style="background-image:url('../img/userImages/lisa.jpg');"></div>
             <p class="profileName">Lisa W.</p>
         </div>
         <div class="date">
