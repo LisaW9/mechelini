da1754d281c31e326cc2083b2e8d73ae20641af4
Lisa9W <lisa@m-release.com>
2017-05-03 11:35:56 +0200

    Started ruilplaza screens

---
+++ includes/ruilkaart.inc.php
@@ -0,0 +1,17 @@
+<div class="ruilkaart">
+    <?php include_once('kaart.inc.php'); ?>
+    <div class="info">
+        <div class="user">
+            <div class="profilePicture" style="background-image"></div>
+            <p class="profileName">Lisa W.</p>
+        </div>
+        <div class="date">
+            <div class="dateIcon"></div>
+            <p class="date">03-mei-17</p>
+        </div>
+        <div class="rarity">
+            <p class="rarityTitle">RARITY</p>
+            <p class="rarity">Common</p>
+        </div>
+    </div>
+</div>
