ad766294aa14bd7aa64b35d02a2bde356d93c1be
Lisa9W <lisa@m-release.com>
2017-05-03 14:09:33 +0200

    Kaart voorbeeld klaar

--- includes/kaart.inc.php
+++ includes/kaart.inc.php
@@ -2,7 +2,4 @@
 <link rel="stylesheet" type="text/css" href="../css/kaart.css">
 
 <div class="kaart">
-    <div class="kaartAfbeelding"></div>
-    <h1 class="kaartNaam">Elias Cobbaut</h1>
-    <h2 class="kaartThema">Spelers</h2>
 </div>
