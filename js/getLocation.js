02177a52c6f3ef8ea0b7d729586262467644c5fa
Lisa9W <lisa@m-release.com>
2017-05-19 20:14:59 +0200

    Finding and comparing location fixed

--- js/getLocation.js
+++ js/getLocation.js
@@ -1,20 +1,20 @@
 $(document).ready(function () {
-    var compareLat = 51.02470267;
-    var compareLon = 4.48420393;
+    var compareLat = 51.024684;
+    var compareLon = 4.484692;
     var latitude;
     var longitude;
 
     navigator.geolocation.getCurrentPosition(function (position) {
-        latitude = position.coords.latitude;
-        longitude = position.coords.longitude;
-
+        latitude = position.coords.latitude.toFixed(6);
+        longitude = position.coords.longitude.toFixed(6);
+        console.log(compareLat + ", " + compareLon);
+        console.log(latitude + ", " + longitude);
+console.log(Math.abs(compareLat - latitude).toFixed(6));
         // TEST -> MOET WEG !!
-        latitude = compareLat;
-        longitude = compareLon;
 
-        if (compareLat == latitude && compareLon == longitude) {
+        if (Math.abs(compareLat - latitude).toFixed(6) < 0.001 && Math.abs(compareLon - longitude).toFixed(6) < 0.001) {
             alert('Je bent in de Gym!!');
-            $.post( "ajax/location.php", function( data ) {
+            $.post("ajax/location.php", function (data) {
                 alert(data);
             });
         } else {
