38b444aa3acded4a98f26736c516d3e07fe10c57
Lisa9W <lisa@m-release.com>
2017-05-21 17:52:34 +0200

    tradable cards shown in overview

--- includes/kaart.inc.php
+++ includes/kaart.inc.php
@@ -1,3 +1,4 @@
+<?php if ($result->trade == 1) echo '<div class="shadow"></div>'; ?>
 <div class="kaart" id="<?php echo str_replace(' ', '-', $result->ucId); ?>"
-     style="background-image: url('img/kaarten/<?php echo $result->image ?>');">
+     style="background-image: url('img/kaarten/<?php echo $result->image ?>'); <?php if ($result->trade == 1) echo 'margin-top: -150px; z-index: 500; position: relative;'; ?>">
 </div>
